16329f8ff3ca2e1b0aebe243b929f748fdca6e20
Dimlock <roshin.artur@yandex.ru>
2025-02-07 21:33:53 +0300

    модель шаблона в json

---
+++ save_load.py
@@ -0,0 +1,55 @@
+import json
+
+def save():
+    test_json = {
+        "template": {
+            "name": "тест",
+            "description": "тестовое описание",
+            "lists": {
+                "relations": {
+                    "categories": [
+                        {
+                            "name": "имя категории",
+                            "elements": []
+                        },
+                        {
+                            "name": "имя второй категории",
+                            "elements": []
+                        }
+                    ]
+                },
+                "other_lists": [
+                    {
+                        "name": "название другого списка",
+                        "categories": [
+                            {
+                                "name": "имя другой категории",
+                                "elements": []
+                            },
+                            {
+                                "name": "имя второй другой категории",
+                                "elements": []
+                            }
+                        ]
+                    },
+                    {
+                        "name": "2nd other list",
+                        "categories": [
+                            {
+                                "name": "2nd list 1st cat",
+                                "elements": []
+                            },
+                            {
+                                "name": "2nd list 2nd cat",
+                                "elements": []
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+    with open("test.json", "w", encoding="UTF-8") as f:
+        #f.write(json.loads(test_json, indent=4))
+        json.dump(test_json, f, indent=4, ensure_ascii=False)
