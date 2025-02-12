f7f64a6777017fc1648ef3f28c4586f010e3e6dc
Dimlock <roshin.artur@yandex.ru>
2025-02-12 17:04:37 +0300

    Списки и сам шаблон

--- template.py
+++ template.py
@@ -5,3 +5,22 @@ class Category:
 
     def add(self, element):
         self.elements.append(element)
+
+
+class TempList:
+    def __init__(self, name):
+        self.name = name
+        self.categories = []
+
+    def add(self, element):
+        self.categories.append(element)
+
+
+class Template:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.lists = []
+
+    def add(self, element):
+        self.lists.append(element)
