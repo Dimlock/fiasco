8f01d8f6efb0e1e532911d7e7ba7f0be6ba54eb4
DimlockTablet <roshin.artur@yandex.ru>
2025-02-16 22:04:30 +0300

    Тестирование сохранения работает

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import save_load
+import template
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,18 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    save_load.save()
+    cat1 = template.Category("test1")
+    cat1.add("blabla")
+    cat1.add("aboba")
+    cat2 = template.Category("test2")
+    cat2.add("krol")
+    cat2.add("zeliboba")
+    l1 = template.TempList("First list")
+    l1.add(cat1)
+    l1.add(cat2)
+    temp1 = template.Template("First template", "я хачю пиццу")
+    temp1.add(l1)
+    print(temp1.save())
+    save_load.save(temp1.save())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
